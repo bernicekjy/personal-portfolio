38a1106e9458e5c61b49e6ecc236009d6cf017bd
bernicekjy <68289591+bernicekjy@users.noreply.github.com>
2023-08-20 14:28:58 +0800

    Added settings to deploy

--- src/constants/index.js
+++ src/constants/index.js
@@ -140,31 +140,18 @@ import {
       ],
     },
     {
-      title: "Web Developer",
-      company_name: "Shopify",
-      icon: shopify,
-      iconBg: "#383E56",
-      date: "Jan 2022 - Jan 2023",
+      title: "Web Designer",
+      company_name: "Nanyang Techonological University",
+      icon: ntu,
+      iconBg: "#fff7cc",
+      date: "May 2023 - May 2023",
       points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
+        "",
         "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
         "Implementing responsive design and ensuring cross-browser compatibility.",
         "Participating in code reviews and providing constructive feedback to other developers.",
       ],
-    },
-    {
-      title: "Full stack Developer",
-      company_name: "Meta",
-      icon: meta,
-      iconBg: "#E6DEDD",
-      date: "Jan 2023 - Present",
-      points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
-      ],
-    },
+    }
   ];
   
   const testimonials = [
