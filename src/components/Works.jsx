8d7f443ce97e4c0d5bc904958439e6d26989269b
bernicekjy <68289591+bernicekjy@users.noreply.github.com>
2023-08-22 16:31:38 +0800

    Update Works.jsx

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -15,7 +15,7 @@ const ProjectCard = ({ index, name, description, tags, image, source_code_link})
           scale: 1,
           speed: 560
         }}
-        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full">
+        className="bg-tertiary p-5 rounded-2xl xs:w-[300px] sm:w-[360px] w-full">
 
           <div className='relative w-full h-[230px]'>
             <img
