99c3e622a2cd7c2c5060b5f7b020b666e2f0477b
bernicekjy <68289591+bernicekjy@users.noreply.github.com>
2023-08-22 23:05:42 +0800

    Update SectionWrapper.jsx

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -7,11 +7,20 @@ import { staggerContainer } from '../components/utils/motion';
 const SectionWrapper = (Component, idName) => 
     //Higher Order Components: a function returning a function
   function HOC(){
+    let boxVariants = {};
+    const isMobile = window.innerWidth < 768;
+    if (isMobile){
+        initialOpacity = {opacity: 1} // show all elements
+    }
+    else{
+        initialOpacity = {opacity: 0}
+    }
+
     return(
     // attributes to apply to all sections
     <motion.section
         variants={staggerContainer()}
-        initial={{opacity: 0}}
+        initial={initialOpacity}
         whileInView={{opacity : 1}}
         viewport={{ once:true, amount:0.25} }
         className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
