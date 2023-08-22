aed90f94e70a7ab851cee85cc13ff28fb5d52a6f
bernicekjy <68289591+bernicekjy@users.noreply.github.com>
2023-08-22 14:54:15 +0800

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} font-semibold text-tertiary`}>Hello, 
-          I'm <span className='text-white font-serif text-[90px]'>Bernice</span>.</h1>
+          I'm <span className='text-white font-serif'>Bernice</span>.</h1>
 
           <p className={`${styles.heroSubText} text-white mt-2`}>An aspiring Software Engineer and Designer</p>
         </div>
